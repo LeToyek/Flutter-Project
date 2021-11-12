a28a42edee23a50cd7cb9b1b4429ccd4c9119959
LeToyek <maulanatoyek@gmail.com>
2021-11-12 16:11:05 +0700

    making simple button

--- commencer/lib/main.dart
+++ commencer/lib/main.dart
@@ -17,9 +17,61 @@ class MyApp extends StatelessWidget {
           title: Text('Hello, world!'),
         ),
         body: Center(
-          child: Text('Hello, world! '),
+          child: BiggerText(
+            text: 'goodbye World !',
+          ),
         ),
       ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+
+  Heading({required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({required this.text});
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          widget.text,
+          style: TextStyle(fontSize: _textSize),
+        ),
+        ElevatedButton(
+          child: Text("Perbesar"),
+          onPressed: () {
+            setState(() {
+              _textSize = _textSize + 2;
+            });
+          },
+        )
+      ],
     );
   }
 }
